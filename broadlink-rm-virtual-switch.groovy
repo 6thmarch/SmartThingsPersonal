e19f6a04b4d1706b3800e6991a09c1ec1749d24f
6thmarch <6thmarch@gmail.com>
2015-12-09 22:24:24 +0800

    Updated Description, References and Instructions

--- broadlink-rm-virtual-switch.groovy
+++ broadlink-rm-virtual-switch.groovy
@@ -3,6 +3,58 @@
  *	
  *	Broadlink™ RM Virtual Switch 
  *	Version : 1.0.0
+ * 
+ * 	Description:
+ * 		Broadlink RM Virtual Switch is a SmartThings Device Type that allows you to turn on or off devices 
+ * 		by sending Infrared or RF signal from Broadlink RM device to control your home devices.
+ * 		This device requires Android RM Bridge to be installed and started in an android device within the 
+ * 		same wi-fi network as the Broadlink RM device.
+ * 
+ * 	Requirements:
+ * 		An android device (Android Box/Tablet/Phone) within the same wi-fi network as the Broadlink RM device, with Android RM Bridge installed and running.
+ * 		Broadlink RM device
+ * 
+ * 	References:
+ * 		Android RM Bridge created by Jochen Ruehl which you can get from http://rm-bridge.fun2code.de
+ * 		Broadlink RM Device: http://www.broadlink.com.cn/en/home-en.html
+ * 
+ * 	Instructions:
+ * 		1. Ensure you have an Android device and Broadlink RM device.
+ * 		2. Make sure your Android device and Broadlink RM device is within the same wi-fi network.
+ * 		3. Install Android RM Bridge on your Android device. You can get the app from the references above.
+ * 		4. After installation, open the app, go to the app settings, enable authentication and set up username and password, and then start the server.
+ * 		5. From the Android RM Bridge reference site, click "Manage Codes" to enter the Android RM Bridge Management Console.
+ * 		6. 	Step 1: Enter the IP Address and Port Number stated in the Android RM Bridge App. Click "Load Devices". Enter 
+ * 				the username and password created in the app.
+ * 		7. 	Step 2: Select the Broadlink RM device you want the signal to be sent from.
+ * 		8. 	Step 3: Enter a code for that Infrared / RF signal from your remote.
+ * 		9.	Step 4: Click "Learn" and press the button on your remote pointing at the Broadlink RM device.
+ * 		10.	Step 5: Click "Test Code" button to ensure that it recorded your signal correctly.
+ * 		11. 	Step 6: Return to the Main page of the Management Console and click "Manage Code Shortcuts"
+ * 		12. 	Step 7: Repeat Step 1.
+ * 		13. 	Step 8: Make sure the code you created is listed.
+ * 		14. Add Broadlink™ RM Virtual Switch device type in SmartThings App/SmartThings Developer API Website.
+ * 		15. In the device Preferences, specify:
+ * 			a. Name of switch
+ * 			b. Android Device domain name or External IP Address (Note: Internal IP Address does not work here) 
+ * 			   If your external IP Address is always changing and you do not have a domain name, you may consider 
+ * 			   using Dynamic DNS Service like no-ip.com .	   
+ * 			   e.g. mydomain.com or x.x.x.x * 				
+ * 			c. Port Number listed in the Android RM Bridge App
+ * 			d. Username created for Android RM Bridge App
+ * 			e. Password created for Android RM Bridge App
+ * 			f. ON Code - Code created in the Android RM Bridge Management Console. 
+ * 				     This code will be sent when switch is turned on.
+ * 			g. OFF Code - Code created in the Android RM Bridge Management Console.
+ * 				     This code will be sent when the switch is turned off.
+ * 		16. That's it! You can now control your Broadlink RM device to send Infrared or RF Signals through SmartThings App.
+ * 		
+ * 		17. To also control this device using Amazon Echo via Voice:
+ * 			a. Go to Amazon Echo SmartApps in SmartThings App and add the device into the list.
+ * 			b. Tell Alexa/Amazon (Amazon Echo) to "Discover New Devices".
+ * 		18. You are done! You can now also control your Broadlink RM device to send Infrared or RF Signals through voice using
+ * 		    Amazon Echo. Just say "Alexa, turn on/off [switch name]".
+ * 
  *
  *	The original licensing applies, with the following exceptions:
  *		1.	These modifications may NOT be used without freely distributing all these modifications freely
@@ -51,7 +103,7 @@ metadata {
     preferences {
        section("RM Bridge Server Configuration"){
        input "server", "text", title: "Server Address",
-              description: "This is the domain or IP Address of the Android RM Bridge Server. e.g. mydomain.com or 192.168.x.x", defaultValue: '',
+              description: "This is the domain name or external IP Address of the Android RM Bridge Server. e.g. mydomain.com or x.x.x.x", defaultValue: '',
               required: true, displayDuringSetup: true
 
        input "port", "text", title: "Port Number",
